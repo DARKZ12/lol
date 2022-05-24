9dec02d9e6bb69566f745b8cb7bb555d8817e7a8
DARKZ12 <96714244+DARKZ12@users.noreply.github.com>
2022-05-23 17:31:35 -0700

    Add files via upload

---
+++ botkrapok.py
@@ -0,0 +1,13 @@
+﻿import random 
+
+chars = "GBXEp7bLEB8qFpqaJnwfl25h5NEnHiIyDtKifZi8vE0OTY2MzQwMzU5NjkxMDcxNTE5123456789"
+
+for i in range(1000)
+first = ''.join((random.choice(chars) for import in range(24)))
+second = ''.join((random.choice(chars) for import in range(6)))
+third = ''.join((random.choice(chars) for import in range(27)))
+
+result = first + "." + second + "." + third 
+
+output = open("output.txt", "a")
+output.write(result + "\n")
